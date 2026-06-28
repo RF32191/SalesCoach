e2fb03ba30cebc738d67fabc33ba40ce2e08ab45
Ryan <Fermoselle>
2026-06-28 16:27:57 -0700

    Initial Commit

---
+++ SalesCoach/SalesCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesCoachApp.swift
+//  SalesCoach
+//
+//  Created by Ryan Joshua Fermoselle on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct SalesCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SalesCoachTests/SalesCoachTests.swift
@@ -0,0 +1,16 @@
+//
+//  SalesCoachTests.swift
+//  SalesCoachTests
+//
+//  Created by Ryan Joshua Fermoselle on 6/28/26.
+//
+
+import Testing
+
+struct SalesCoachTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
